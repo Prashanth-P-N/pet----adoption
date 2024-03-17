d58b2f808b5b3552cea70c2845016e8b1c5a7ae1
pavan <prashanth.pn.20@gmail.com>
2024-03-17 22:13:51 +0530

    java come it

---
+++ start.js
@@ -0,0 +1,5 @@
+async function start() {
+  const wetherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
+  const wetherData = await wetherPromise.json();
+  const ourTmperature, wetherData, properties, peripds, []; 0; Tmperature;
+}
