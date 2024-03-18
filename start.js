b550c394bde593165a6b60c59e8043a77eb79654
pavan <prashanth.pn.20@gmail.com>
2024-03-18 21:40:24 +0530

    js update

--- start.js
+++ start.js
@@ -1,9 +1,19 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+
+
 
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
   console.log(petsData)
+  petsData.forEach(pet => {
+    const clone = template.content.cloneNode(true)
+    wrapper.appendChild(clone)
+  });
 
+  document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea()
