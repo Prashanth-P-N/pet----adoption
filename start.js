ce0a3e9ece6f0b4b1e99abcd37ca48c3a7755f4d
pavan <prashanth.pn.20@gmail.com>
2024-03-18 20:54:03 +0530

    js update

--- start.js
+++ start.js
@@ -1,1 +1,9 @@
 
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  console.log(petsData)
+
+}
+
+petsArea()
