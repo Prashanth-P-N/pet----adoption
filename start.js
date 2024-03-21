d6211d1f38f4990d8d3b8a6633fb333ffd9ebc8e
pavan <prashanth.pn.20@gmail.com>
2024-03-21 22:33:39 +0530

    java

--- start.js
+++ start.js
@@ -1,20 +1,9 @@
-const template = document.querySelector("#pet-card-template")
-const wrapper = document.createDocumentFragment()
-
-
-
-
-async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
-  const petsData = await petsPromise.json()
-  console.log(petsData)
-  petsData.forEach(pet => {
-    const clone = template.content.cloneNode(true)
-    clone.querySelector("h3").textContent = pet.name
-    wrapper.appendChild(clone)
-  });
-
-  document.querySelector(".list-of-pets").appendChild(wrapper)
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+  console.log()
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperatur-output").textContent = ourTemperature
+  console.log(ourTemperature)
 }
-
-petsArea()
+start()
